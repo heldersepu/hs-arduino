8735d9589e08b9dbef74d34af498be454180db2e
heldersepu <heldersepu@41a01311-c1dc-be89-260c-0299d428579e>
2013-11-05 01:10:47 +0000

    Add a wifi shield

--- EasyLights/receiver/receiver.ino
+++ EasyLights/receiver/receiver.ino
@@ -1,4 +1,5 @@
-const int BoardID = '3';
+#include <WiServer.h>
+const int BoardID = 3;
 const int pinMax = 5;
 
 struct leds {
@@ -12,6 +13,21 @@ int setup_status = 0;
 unsigned long lastCycle = 0;
 boolean cyclicState = LOW;
 
+// Wireless configuration parameters ----------------------------------------
+unsigned char local_ip[] = {192,168,1, BoardID};   // IP address of WiShield
+unsigned char gateway_ip[] = {192,168,1,1};	   // router or gateway IP address
+unsigned char subnet_mask[] = {255,255,255,0};     // subnet mask for the local network
+const prog_char ssid[] PROGMEM = {"ARDUINO"};      // max 32 bytes
+
+unsigned char security_type = 0;	// 0 - open; 1 - WEP; 2 - WPA; 3 - WPA2
+
+// WPA/WPA2 passphrase
+const prog_char security_passphrase[] PROGMEM = {"udinese10"};	// max 64 characters
+prog_uchar wep_keys[] PROGMEM = { };
+unsigned char wireless_mode = 2;
+unsigned char ssid_len;
+unsigned char security_passphrase_len;
+// End of wireless configuration parameters ----------------------------------------
 
 void checkInput(int inByte){
     switch (inByte) {
@@ -55,13 +71,33 @@ void do_the_lights() {
     }
 }
 
+boolean serveFunction(char* URL) {  
+    if (strcmp(URL, "/") == 0) {
+        WiServer.print("Hello World!");
+        turnAll(HIGH);        
+        return true;
+    }
+    return false;
+}
+
+void printData(char* data, int len) {
+    while (len-- > 0) {
+        Serial.print(*(data++));
+    }
+}
+
 void setup() {
     Serial.begin(9600);
+    Serial.println("INIT");
     Serial.println(BoardID);
     // initialize the LED pins as output:
     for (int i = 0; i < pinMax; i++) {
         pinMode(Leds[i].pin, OUTPUT);
     }
+    // initialize the serve function
+    WiServer.init(serveFunction);
+    WiServer.enableVerboseMode(true);
+    Serial.println("LOOP");
 }
 
 void loop(){
@@ -76,6 +112,8 @@ void loop(){
         checkInput(inSerial);
     }
 
+    WiServer.server_task(); 
     do_the_lights();
+    delay(10);    
 }
 
