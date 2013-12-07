e0eddf7d19a9e69ea028fe7a925f36eddb4c9b7b
heldersepu <heldersepu@41a01311-c1dc-be89-260c-0299d428579e>
2013-12-07 19:00:01 +0000

    Add the lights logic to the transmitter

--- EasyLights/XBee/transmitter/transmitter.ino
+++ EasyLights/XBee/transmitter/transmitter.ino
@@ -1,7 +1,9 @@
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial(13, 12); // RX, TX
 
-boolean sample_output = true;
+int setup_status = 0;
+boolean verbose_output = false;
+boolean sample_output = false;
 long updateTime = 0;
 int samplePin = 0;
 
@@ -17,8 +19,53 @@ struct input {
   char value;
 };
 
-input Input[pinMax] = {{17, 0, 1, 'B'}, {18, 0, 0, 'H'}, {19, 0, 0, 'L'}, {20, 0, 1, 'A'}, {21, 0, 1, 'B'},
+input Input[pinMax] = {{17, 0, 1, 'B'}, {18, 0, 0, 'H'}, {19, 0, 0, 'L'}, {20, 0, 1, 'A'}, {21, 0, 1, 'S'},
                        {22, 0, 0, 'C'}, {23, 0, 0, 'F'}, {24, 0, 0, 'I'}, {22, 0, 0, 'M'}, {22, 0, 0, 'R'}};
+
+
+void checkInput(int inByte) {
+    if (verbose_output) Serial.println(inByte);
+    switch (inByte) {
+        case 49:
+            //turnAll(HIGH);
+            break;
+        case 48:
+            //turnAll(LOW);
+            break;
+        case 86:
+            doVerbose();
+            break;
+        case 87:
+            doStatus();
+            break;
+        case 88:
+            sample_output = !sample_output;
+            break;
+        default:
+            if (verbose_output) {
+                Serial.print("checkInput: ");
+                Serial.println(inByte);
+            }
+    }
+}
+
+void doVerbose() {
+    verbose_output = !verbose_output;
+    Serial.print("verbose: ");
+    Serial.println(verbose_output);
+}
+
+void doStatus() {
+    for (int i = 0; i < pinMax; i++) {
+        Serial.print(Input[i].pin);
+        Serial.print(":");
+        Serial.print(Input[i].on);
+        Serial.print(".");
+        Serial.print(Input[i].value);
+        Serial.print("; ");
+    }
+    Serial.println("");
+}
 
 void doSample() {
     char mystr[5] = "\10\10\10\10";
@@ -47,20 +94,79 @@ void doSample() {
     Serial.println("*********");
 }
 
+void doLights() {
+    char mystr[pinMax+2];
+    boolean iStop = false;
+    for (int i = 0; i < pinMax+2; i++) {
+        mystr[i] = 10;
+    }
+    int j = 0;
+    mystr[0] = 'K';
+    for (int i = 0; i < pinMax; i++) {
+        if (Input[i].on) {
+            if (Input[i].cyclic) {
+                if ((millis() - lastCycle) > 600) {
+                    lastCycle = millis();
+                    cyclicState = !cyclicState;
+                }
+                if (cyclicState) {
+                    mystr[j++] = Input[i].value;
+                }
+            } else {
+                mystr[j++] = Input[i].value;
+            }
+            if (mystr[j-1] == 'S') {
+                iStop = true;
+            }
+        }
+    }
+    
+    if (iStop) {
+        mystr[j++] = 'T';
+        mystr[j++] = 'P';
+    }
+    mySerial.write(mystr);
+    
+    if (verbose_output) {
+        for (int i = 0; i < pinMax+2; i++) {
+            Serial.print(mystr[i]);
+            Serial.print(',');
+        }
+        Serial.println("*********");
+    }
+}
+
+void errFlash(int mTime, int mDelay) {
+    if (millis() < mTime) {
+        //turnAll(!Leds[0].on);
+        delay(mDelay);
+    }
+}
+
 void setup() {
     Serial.begin(9600);
     Serial.println("INIT");
     mySerial.begin(9600);
+    
+    for (int i = 0; i < pinMax; i++) {
+       pinMode(Input[i].pin, INPUT_PULLUP);
+    }
 }
 
 void loop() {
-    if (mySerial.available())
-        Serial.write(mySerial.read());
-    if (Serial.available())
-        mySerial.write(Serial.read());
+    // check input from the serial port.
+    while (Serial.available()) {
+        checkInput(Serial.read());
+    }
+    
+    // check the switches.
+    for (int i = 0; i < pinMax; i++) {
+       Input[i].on = !digitalRead(Input[i].pin);
+    }
 
     if (sample_output) {
         doSample();
+    } else {
+        doLights();
     }
-
 }
