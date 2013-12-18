b00425fd49c66276f7f72e8a09019b23587b995b
heldersepu <heldersepu@41a01311-c1dc-be89-260c-0299d428579e>
2013-12-18 14:26:55 +0000

    testing eeprom read and write

---
+++ trunk/eeprom_io/eeprom_io.ino
@@ -0,0 +1,28 @@
+#include <EEPROM.h>
+char MasterKey[8] = "123EASY";
+
+void writeMasterKey() {
+    for (int i = 1; i < 8; i++) {
+        EEPROM.write(i, MasterKey[i-1]);
+    }  
+}
+
+void setup() {
+    Serial.begin(9600);
+
+    char TempKey[8] = "-------";
+    for (int i = 1; i < 8; i++) {
+        TempKey[i-1] = EEPROM.read(i); 
+    }
+
+    if (strcmp(TempKey, MasterKey) == 0) {
+        Serial.println(TempKey);      
+    } else {
+        Serial.println("writeMasterKey");
+        writeMasterKey();
+    }  
+}
+
+void loop() {
+    delay(500);
+}
