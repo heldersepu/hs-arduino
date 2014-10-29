3f577bc363ce61fc6bef0fd47f7c0051e6f6cec3
heldersepu <heldersepu@41a01311-c1dc-be89-260c-0299d428579e>
2014-10-29 18:12:42 +0000

    Extend the LoopLights.ino to read from the serial

--- trunk/LoopLights/LoopLights.ino
+++ trunk/LoopLights/LoopLights.ino
@@ -1,8 +1,23 @@
 const int pinLed[3] = {2, 4, 6};
 const int pinMax = sizeof(pinLed)/sizeof(int);
+boolean doLights = true;
+int lspeed = 250;
 
+void turnAll(boolean value){
+  for (int i = 0; i < pinMax; i++) {
+    digitalWrite(pinLed[i], value);
+  }
+}
+
+void doOnOff(int portNum){
+  digitalWrite(portNum, HIGH);
+  delay(lspeed*2);
+  digitalWrite(portNum, LOW);
+  delay(lspeed);
+}
 
 void setup() {
+  Serial.begin(9600);
   // initialize the LED pin as an output:
   for (int i = 0; i < pinMax; i++) {
     pinMode(pinLed[i], OUTPUT);
@@ -11,10 +26,12 @@ void setup() {
 
 void loop(){   
   for (int i = 0; i < pinMax; i++) {
-    digitalWrite(pinLed[i], HIGH);
-    delay(500);
-    digitalWrite(pinLed[i], LOW);
-    delay(250);
+    if (Serial.available()) {
+      checkInput(Serial.read());
+    }
+    if (doLights) {
+      doOnOff(pinLed[i]);      
+    }
   }  
 }
 
