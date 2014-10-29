6221a512c32fb24e8f1d384ca7354e1c5ad3fa1c
heldersepu <heldersepu@41a01311-c1dc-be89-260c-0299d428579e>
2014-10-29 00:24:02 +0000

    Simple code to loop through some LED

---
+++ trunk/LoopLights/LoopLights.ino
@@ -0,0 +1,20 @@
+const int pinLed[3] = {2, 4, 6};
+const int pinMax = sizeof(pinLed)/sizeof(int);
+
+
+void setup() {
+  // initialize the LED pin as an output:
+  for (int i = 0; i < pinMax; i++) {
+    pinMode(pinLed[i], OUTPUT);
+  }  
+}
+
+void loop(){   
+  for (int i = 0; i < pinMax; i++) {
+    digitalWrite(pinLed[i], HIGH);
+    delay(500);
+    digitalWrite(pinLed[i], LOW);
+    delay(250);
+  }  
+}
+
