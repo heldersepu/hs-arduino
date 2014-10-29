f82966c6270a223ec060bb0b2d18ee11db3e835e
heldersepu <heldersepu@41a01311-c1dc-be89-260c-0299d428579e>
2014-10-29 18:12:42 +0000

    Extend the LoopLights.ino to read from the serial

---
+++ LoopLights/CheckInput.ino
@@ -0,0 +1,35 @@
+void checkInput(int inByte){
+  switch (inByte) {
+  case 49:
+    Serial.println("ON");
+    doLights = false;
+    turnAll(HIGH);
+    delay(500);
+    break;
+  case 48:
+    Serial.println("OFF");
+    doLights = false;
+    turnAll(LOW);
+    break;
+  case 97: // a
+  case 98: // b
+  case 99: // c
+    doLights = false;
+    doOnOff(pinLed[inByte-97]);
+    delay(250);
+    break;
+  case 102: // f
+    lspeed = 50;
+    break;
+  case 115: // s
+    lspeed = 250;
+    doLights = !doLights;
+    break;
+  case 116: // t
+    Serial.print("\nBoard Time: ");
+    Serial.println(millis());
+    break;
+  default:
+    Serial.println(inByte);
+  }
+}
