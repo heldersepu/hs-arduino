971f54b50801e659701a09be9a278357a8e6f1a6
Helder Sepulveda <heldersepu@gmail.com>
2018-10-19 22:42:32 -0400

    Start moving on input

--- SmartServos/Servo.h
+++ SmartServos/Servo.h
@@ -1,9 +1,18 @@
-#include <Stepper.h> 
+#include <Stepper.h>
 #define STEPS 32 // steps on the motor
 
+
 class Servo : public Stepper {
-  public: 
+  public:
     Servo(int pin) : Stepper(STEPS, pin, pin+2, pin+1, pin+3) {
       setSpeed(200);
-    }  
+    }
+
+    void move(char x) {
+      if (x < 'a') {
+        step(100);
+      } else {
+        step(-100);
+      }
+    }
 };
--- SmartServos/SmartServos.ino
+++ SmartServos/SmartServos.ino
@@ -4,18 +4,31 @@ const int MAX = 2;
 Servo motors[MAX] = {{22}, {30}};
 
 void setup() {
-  Serial.begin(9600);  
+  Serial.begin(9600);
 }
 
 void loop() {
   if (Serial.available()>0) {
-    start(Serial.parseInt());    
-  }   
+    start(Serial.readString());
+  }
 }
 
-void start(int val) {
+void start(String val) {
   Serial.println(val);
-  for (int i = 0; i < MAX; i++) {
-    motors[i].step(val);  
+  for (int i = 0; i < val.length(); i+=2) {
+    if (val[i] > 10) {
+      action(val[i],val[i+1]);
+    }
+  }
+}
+
+void action(char k, char v) {
+  if (k >= '0' && k <= '9') {
+    motors[k - '0'].move(v);
+  } else if (k == '.'){
+    Serial.println("sleep");
+  } else {
+    Serial.print("Invalid Key=");
+    Serial.println(k);
   }
 }
